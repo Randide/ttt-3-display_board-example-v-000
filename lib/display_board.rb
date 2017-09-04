c90c2e034f33c524987167ca63c306f712f1cbbe
Randi Nash <randidena@live.com>
2017-09-04 00:05:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 def display_board
-  puts = "A Tic Tac Toe Board"
-  puts = "   ""|""   ""|""   "
-  puts = "-----------"
-  puts = "   ""|""   ""|""   "
-  puts = "-----------"
-  puts = "   ""|""   ""|""   "
+  puts "A Tic Tac Toe Board"
+  puts "   ""|""   ""|""   "
+  puts "-----------"
+  puts "   ""|""   ""|""   "
+  puts "-----------"
+  puts "   ""|""   ""|""   "
 end
 
 display_board
